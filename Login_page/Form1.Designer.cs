bad8a6afe096013c771011fbaf9f529f0ad0e81f
Safkat khan Tafsir <safkatkhan420@gmail.com>
2023-11-18 01:04:41 +0600

    log in page added

--- Login_page/Form1.Designer.cs
+++ Login_page/Form1.Designer.cs
@@ -28,78 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             label1 = new Label();
             label2 = new Label();
+            label3 = new Label();
             textBox1 = new TextBox();
             textBox2 = new TextBox();
-            label3 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            pictureBox1 = new PictureBox();
+            label4 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(694, 179);
-            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Location = new Point(697, 90);
             label1.Name = "label1";
-            label1.Size = new Size(119, 25);
+            label1.Size = new Size(182, 20);
             label1.TabIndex = 0;
-            label1.Text = "TicketBengal";
+            label1.Text = "Welcome To Tourist Ticket";
             label1.Click += label1_Click;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(589, 308);
-            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Location = new Point(579, 158);
             label2.Name = "label2";
-            label2.Size = new Size(63, 25);
+            label2.Size = new Size(82, 20);
             label2.TabIndex = 1;
-            label2.Text = "label2";
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(712, 304);
-            textBox1.Margin = new Padding(4);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(155, 32);
-            textBox1.TabIndex = 2;
-            textBox1.TextChanged += textBox1_TextChanged;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(786, 409);
-            textBox2.Margin = new Padding(4);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(155, 32);
-            textBox2.TabIndex = 3;
+            label2.Text = "Username :";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(611, 444);
-            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Location = new Point(584, 230);
             label3.Name = "label3";
-            label3.Size = new Size(63, 25);
-            label3.TabIndex = 4;
-            label3.Text = "label3";
+            label3.Size = new Size(77, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Password :";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(679, 155);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(227, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(679, 223);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(227, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(893, 0);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(86, 31);
+            button1.TabIndex = 5;
+            button1.Text = "Admin";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(679, 305);
+            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Name = "button2";
+            button2.Size = new Size(227, 31);
+            button2.TabIndex = 6;
+            button2.Text = "LogIn";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(679, 413);
+            button3.Margin = new Padding(3, 4, 3, 4);
+            button3.Name = "button3";
+            button3.Size = new Size(227, 31);
+            button3.TabIndex = 7;
+            button3.Text = "SignUp";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-9, 0);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(561, 616);
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(778, 370);
+            label4.Name = "label4";
+            label4.Size = new Size(26, 20);
+            label4.TabIndex = 9;
+            label4.Text = "or,";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.ButtonHighlight;
-            ClientSize = new Size(1552, 736);
-            Controls.Add(label3);
+            ClientSize = new Size(982, 616);
+            Controls.Add(label4);
+            Controls.Add(pictureBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
+            Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            ForeColor = SystemColors.InfoText;
-            Margin = new Padding(4);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
-            Text = "TicketBengal";
-            Load += Form1_Load;
+            Text = "Ticket With C#";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -108,8 +162,13 @@
 
         private Label label1;
         private Label label2;
+        private Label label3;
         private TextBox textBox1;
         private TextBox textBox2;
-        private Label label3;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private PictureBox pictureBox1;
+        private Label label4;
     }
 }
--- Login_page/Form1.cs
+++ Login_page/Form1.cs
@@ -7,17 +7,7 @@ namespace Login_page
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
         private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
